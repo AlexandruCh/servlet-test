6ff0e2fa467ea0de97cb0dd86dd10d85e64344eb
AlexandruCh <34208988+AlexandruCh@users.noreply.github.com>
2022-03-21 08:55:50 +0200

    Update Test.java

--- src/main/java/com/example/test/Test.java
+++ src/main/java/com/example/test/Test.java
@@ -6,23 +6,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Test
- */
+
+//USBean("QE-77")
 public class Test extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public Test() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	//TCBean("QE-T4")
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Welcome to : ").append("servlet last");
